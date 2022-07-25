82034e5a9b9f9514251c5ea2536509b45c3ff926
Davide Giusti <99611360+DavideGiusti87@users.noreply.github.com>
2022-07-25 18:06:09 +0200

    Initial commit

---
+++ src/test/java/it/develhope/TimeBank/TimeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.TimeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

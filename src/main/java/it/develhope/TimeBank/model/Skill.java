613aea4f38982462f26b41c0fc4d540572055c7e
Angelo <angelofag3095@gmail.com>
2022-08-25 16:39:58 +0200

    Creazione classe e repository skill

--- src/main/java/it/develhope/TimeBank/model/Skill.java
+++ src/main/java/it/develhope/TimeBank/model/Skill.java
@@ -1,4 +1,43 @@
 package it.develhope.TimeBank.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String description;
+
+    public Skill() {
+    }
+
+    public Skill(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
 }

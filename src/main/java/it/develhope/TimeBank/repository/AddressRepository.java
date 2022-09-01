7ecef2b919dfdf50076917e3b2c7a4ec566e60e5
Angelo <angelofag3095@gmail.com>
2022-09-01 13:27:47 +0200

    merged CRUDAddress branch into dev

---
+++ src/main/java/it/develhope/TimeBank/repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package it.develhope.TimeBank.repository;
+
+import it.develhope.TimeBank.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}

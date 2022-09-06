ccc32d94e4757c9ea6239b5e2269de53a2ba636e
Raman <romadmukhouski@gmail.com>
2022-09-06 14:55:56 +0200

    added @Repository to VolunteerRepository

--- src/main/java/it/develhope/TimeBank/repository/VolunteerRepository.java
+++ src/main/java/it/develhope/TimeBank/repository/VolunteerRepository.java
@@ -2,6 +2,8 @@ package it.develhope.TimeBank.repository;
 
 import it.develhope.TimeBank.entities.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VolunteerRepository extends JpaRepository <Volunteer,Long>{
 }

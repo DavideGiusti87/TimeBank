cd7b2c95124f2f34e63f92db5d767a572d20c04a
Raman <romadmukhouski@gmail.com>
2022-09-06 16:00:44 +0200

    ResponseEntity

--- src/main/java/it/develhope/TimeBank/service/UserService.java
+++ src/main/java/it/develhope/TimeBank/service/UserService.java
@@ -17,58 +17,76 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public ResponseEntity insertNewUser(User user){
-        user.setId(null);
-        userRepository.save(user);
-        //return an HTTP Response with the exit of the operation
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    public List<User> getAllUsers() {
-        //API for fetching the list of all users
-        List<User> allUsers = userRepository.findAll();
-        return allUsers;
-    }
-
-    public User getUserById(Long Id) {
-        Optional<User> user = userRepository.findById(Id);
-        return user.get();
-    }
-
-    public User getUserByUsername(String username) {
-        //API for fetching a single user by username
-        /**
-         * Qui usiamo optional senza il IF
-          */
-        Optional<User> user = userRepository.findByName(username);
-            return user.get();
-    }
-
-
-    public ResponseEntity updateUser(Long id, User updatedUser) {
-        // API for updating User's informations
-        // return an HTTP Response with the exit of the operation
-        if(!userRepository.existsById(id)){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        } else {
-            userRepository.save(updatedUser);
-            return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity insertNewUser (User user){
+        try{
+            user.setId(null);
+            userRepository.save(user);
+            //Primo metodo di ritorno di ResponseEntity
+            return ResponseEntity.status(HttpStatus.OK).build();}
+        catch(Exception ex){
+            //Secondo metodo di ritorno di ResponseEntity
+            return ResponseEntity.badRequest().body(ex.getMessage());
         }
     }
 
+    public ResponseEntity <List<User>> getAllUsers() {
+
+        List<User> allUsers = userRepository.findAll();
+        if(allUsers !=null){
+            //Terzo metodo di ritorno di ResponseEntity
+            //Quale è meglio usare?
+            return new ResponseEntity<>(allUsers, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity <User> getUserById(Long Id) {
+        try{
+            Optional <User> user = userRepository.findById(Id);
+            return new ResponseEntity<>(user.get(),HttpStatus.OK);
+        }
+        catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    public ResponseEntity <User> getUserByUsername(String username) {
+        try {
+            Optional <User> user = userRepository.findByName(username);
+            return new ResponseEntity<>(user.get(),HttpStatus.OK);
+        }catch(Exception ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    public ResponseEntity updateUser(Long id, User updatedUser) {
+        if(!userRepository.existsById(id)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        userRepository.save(updatedUser);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
     public ResponseEntity deleteUserByUsername(String username) {
-        // API for deleting a User by username
-        // return an HTTP Response with the exit of the operation
+        if(username == null){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
         userRepository.deleteByUsername(username);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @DeleteMapping("/deleteUserById")
     public ResponseEntity deleteUserById(@RequestParam Long id) {
-        //API for deleting a User by id
-        //return an HTTP Response with the exit of the operation
+        if(id == null){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
         userRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    @DeleteMapping("/deleteAllUsers")
+    public ResponseEntity deleteAllUsers(){
+        userRepository.deleteAll();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }

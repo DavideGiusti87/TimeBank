5e72c39f9363940fcb1d2f33a2355fd79da78eac
nsgiunta <nsgiunta@gmail.com>
2022-09-07 10:22:40 +0200

    swagger config and api operations in controllers

--- src/main/java/it/develhope/TimeBank/controllers/RequestController.java
+++ src/main/java/it/develhope/TimeBank/controllers/RequestController.java
@@ -1,5 +1,6 @@
 package it.develhope.TimeBank.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import it.develhope.TimeBank.entities.Skill;
 import it.develhope.TimeBank.repository.RequestRepository;
 import it.develhope.TimeBank.entities.request.Request;
@@ -24,41 +25,49 @@ public class RequestController {
     private UserRepository userRepository;
 
    @PostMapping("/new")
+   @ApiOperation(value = "new request", notes = "create a new request")
    public ResponseEntity<Request> addRequest(@RequestBody Request request){
        return requestService.createRequest(request);
    }
 
     @GetMapping("/fetchAll")
+    @ApiOperation(value = "fetchAll", notes = "get all the request")
     public ResponseEntity<List<Request>> getAllRequests() {
         return requestService.getAllRequests();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "id", notes = "get request by id")
     public ResponseEntity<Request> getById(@PathVariable Long id){
         return requestService.getRequestById(id);
     }
 
     @GetMapping("/{skill}")
+    @ApiOperation(value = "skill", notes = "get request by skill")
     public ResponseEntity<Request> getBySkill(@PathVariable Skill skill){
        return requestService.getRequestBySkill(skill);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "id", notes = "update request info by id")
     public ResponseEntity<Request> updateRequest(@PathVariable Long id, @RequestBody Request request){
        return requestService.updateRequest(id, request);
     }
 
     @DeleteMapping("/{username}")
+    @ApiOperation(value = "username", notes = "delete username's request")
     public ResponseEntity<Request> deleteRequestsByUsername(@PathVariable String username) {
         return requestService.deleteByUsername(username);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "id", notes = "delete request by id")
     public ResponseEntity<Request> deleteById(@PathVariable Long id){
        return requestService.deleteById(id);
     }
 
     @DeleteMapping("/deleteAll")
+    @ApiOperation(value = "deleteAll", notes = "delete all the requests")
     public ResponseEntity<Request> deleteAllRequests(){
        return requestService.deleteAll();
     }

85c4a286d1e669e2a6abdc7e48d7318c0fe445a2
[Chiara] <chiara.galatro@gmail.com>
2022-09-08 15:23:30 +0200

    CRUD Volunteer

--- src/main/java/it/develhope/TimeBank/repository/VolunteerRepository.java
+++ src/main/java/it/develhope/TimeBank/repository/VolunteerRepository.java
@@ -1,9 +1,13 @@
 package it.develhope.TimeBank.repository;
 
+import it.develhope.TimeBank.entities.Skill;
 import it.develhope.TimeBank.entities.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VolunteerRepository extends JpaRepository <Volunteer,Long>{
+    Optional<Volunteer> findBySkill(Skill skill);
 }

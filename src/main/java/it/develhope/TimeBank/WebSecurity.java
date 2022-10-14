e06b4199add27f59a553c5432baa2474f5326d4c
Davide Giusti <99611360+DavideGiusti87@users.noreply.github.com>
2022-10-14 10:26:56 +0200

    add WebSecurity.java

--- src/main/java/it/develhope/TimeBank/WebSecurity.java
+++ src/main/java/it/develhope/TimeBank/WebSecurity.java
@@ -1,11 +1,39 @@
 package it.develhope.TimeBank;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true,jsr250Enabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/requests/**").hasAnyRole("ROLE_"+Roles.REGISTERED)
+                .anyRequest().authenticated();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 
 }

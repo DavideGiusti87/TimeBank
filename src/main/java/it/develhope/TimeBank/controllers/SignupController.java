932d7b77842f807831a6ce48704efca9b35be94a
Angelo <angelofag3095@gmail.com>
2022-10-14 11:40:06 +0200

    logs added to SignupController controllers

--- src/main/java/it/develhope/TimeBank/controllers/SignupController.java
+++ src/main/java/it/develhope/TimeBank/controllers/SignupController.java
@@ -4,6 +4,8 @@ import it.develhope.TimeBank.model.DTO.SignupActivationDTO;
 import it.develhope.TimeBank.model.DTO.SignupDTO;
 import it.develhope.TimeBank.model.entities.User;
 import it.develhope.TimeBank.service.SignupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +20,28 @@ public class SignupController {
     @Autowired
     private SignupService signupService;
 
+    private static Logger logger = LoggerFactory.getLogger(SignupController.class);
+
     @PostMapping("/signup")
-    public User signup(@RequestBody SignupDTO signupDTO) throws Exception {
-        return signupService.signup(signupDTO);
+    public ResponseEntity signup(@RequestBody SignupDTO signupDTO) throws Exception {
+        try {
+            logger.info("Registration successful!");
+            return ResponseEntity.status(HttpStatus.OK).body(signupService.signup(signupDTO));
+        }catch (Exception ex){
+            logger.error(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
     }
 
     @PostMapping("/signup/activation")
-    public User activation(@RequestBody SignupActivationDTO signupActivationDTO) throws Exception {
-        return signupService.activate(signupActivationDTO);
+    public ResponseEntity activation(@RequestBody SignupActivationDTO signupActivationDTO) throws Exception {
+        try {
+            logger.info("User activated successfully!");
+            return ResponseEntity.status(HttpStatus.OK).body(signupService.activate(signupActivationDTO));
+        }catch (Exception ex){
+            logger.error(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
 
     }
 
